d1846954aa4a938437007041bbe21741fb622b18
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-20 20:12:11 -0500

    working on Home.js

---
+++ public/scripts/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//Home Page view  
+export default React.createClass({
+	render: function () {
+		return (
+			<section>
+				<h1>Home</h1>
+			</section>
+		);
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
